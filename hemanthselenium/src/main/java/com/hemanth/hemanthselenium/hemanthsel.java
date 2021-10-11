334303c9082f155c877daa98f11dcbe08aef789b
Slim <Slim@Admin>
2021-10-11 16:20:48 +0530

    Hamnth sel push

---
+++ hemanthselenium/src/main/java/com/hemanth/hemanthselenium/hemanthsel.java
@@ -0,0 +1,13 @@
+package com.hemanth.hemanthselenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class hemanthsel 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
